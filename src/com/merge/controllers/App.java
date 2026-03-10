4a8a2083110925d52d85479d35af4ad61e9509c3
Meromah <124768973+meromah@users.noreply.github.com>
2026-03-10 08:00:31 +0100

    a wrapper controller class App has been created to move orchestaring logic and operations out from Main class;

--- src/com/merge/controllers/App.java
+++ src/com/merge/controllers/App.java
@@ -2,12 +2,23 @@ package com.merge.controllers;
 
 import com.merge.services.BootstrapService;
 import java.io.File;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.sql.Connection;
+import com.merge.services.SQLiteService;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class App {
     public static void run() {
-        // get appDir
-        File appDir = BootstrapService.getAppDir();
-        // check for configuration file and database
-        File configurationFile = new FIle();
+
+        BootstrapService bootstrap = new BootstrapService();
+        Path configPath = bootstrap.getConfigPath();
+
+        Properties config = bootstrap.getConfig();
+
+        Connection connection = SQLiteService.getConnection(config);
+        System.exit(0);
     }
 }
