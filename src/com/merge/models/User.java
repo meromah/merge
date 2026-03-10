a5ac1c17c7027dfa5c9f8502301bf723ac4b9c0d
Meromah <124768973+meromah@users.noreply.github.com>
2026-03-10 12:18:22 +0100

    added more fields and notes; setter and getter methods were added as well;

--- src/com/merge/models/User.java
+++ src/com/merge/models/User.java
@@ -3,11 +3,20 @@ package com.merge.models;
 import java.util.Properties;
 import com.merge.exceptions.UserNotFoundException;
 import com.merge.exceptions.UserNotFullException;
+import java.security.PublicKey;
+import com.merge.models.Address;
+import java.time.ZonedDateTime;
 
 public class User {
     private String name;
+    private Address address;
     private String bio;
-    private Properties config;
+    private final Properties config;
+    private PublicKey publicKey;
+    private ZonedDateTime lastOnline;
+    // add hashid or private key signaure when private key is set
+    // upon save method save this to db
+    // create db table users with the given fields
 
     public User(String name, String bio, Properties config) {
         this.name = name;
@@ -15,17 +24,20 @@ public class User {
         this.config = config;
     }
 
-    public static User load(Properties config) {
-        String name = config.getProperty("profile.name");
-        String bio = config.getProperty("profile.bio");
-        if (name == null || name.isBlank()) {
-            throw new UserNotFoundException();
-        }
-        return new User(name, bio, config);
-    }
+    // public static User load(Properties config) {
+    //     String name = config.getProperty("profile.name");
+    //     String bio = config.getProperty("profile.bio");
+    //     if (name == null || name.isBlank()) {
+    //         throw new UserNotFoundException();
+    //     }
+    //     return new User(name, bio, config);
+    // }
 
     public String getName() { return this.name; }
     public String getBio() { return this.bio; }
+    public Address getAddress() {return this.address;}
+    public PublicKey getPublicKey() {return this.publicKey;}
+    public ZonedDateTime getLastOnline() {return this.lastOnline;}
 
     public User setName(String name) {
         this.name = name;
@@ -36,6 +48,22 @@ public class User {
         this.bio = bio;
         return this;
     }
+
+    public User setAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public User setPublicKey(PublicKey publicKey) {
+        this.publicKey = publicKey;
+        return this;
+    }
+
+    public User setLastOnline(ZonedDateTime lastOnline) {
+        this.lastOnline = lastOnline;
+        return this;
+    }
+
 
     public void save() {
         if (this.name == null || this.name.isBlank()) {
