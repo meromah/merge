8198f904ef6a8fe6e20d8d85cc8656684d7e6f2a
Meromah <124768973+meromah@users.noreply.github.com>
2026-03-10 12:17:03 +0100

    two models created;
    Address model will be used to represent network address socket;
    Profile model, extending User model, will represent the user to
    himself/herself;

---
+++ src/com/merge/models/Address.java
@@ -0,0 +1,21 @@
+package com.merge.models;
+
+public class Address {
+    private String ip;
+    private int port;
+
+    public Address(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public Address setIp(String ip) {
+        this.ip = ip;
+        return this;
+    }
+
+    public Address setPort(int port) {
+        this.port = port;
+        return this;
+    }
+}
---
+++ src/com/merge/models/Profile.java
@@ -0,0 +1,14 @@
+package com.merge.models;
+
+import com.merge.models.User;
+import java.util.Properties;
+import java.security.PrivateKey;
+
+public class Profile extends User {
+    public Profile(String name, String bio, Properties config) {
+        super(name, bio, config);
+    }
+
+    // public void save(proatpeite config)
+    //     save to congi all
+}
