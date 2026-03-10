86f1998541274b33e8464bcc59ca5d92df98eb41
Meromah <124768973+meromah@users.noreply.github.com>
2026-03-10 08:18:03 +0100

    sqllite now has extra feature for managing connection;
    the service is not instantiated and its function are non-static;

--- src/com/merge/services/SQLiteService.java
+++ src/com/merge/services/SQLiteService.java
@@ -7,18 +7,37 @@ import com.merge.services.Service;
 import java.util.Properties;
 
 public class SQLiteService extends Service {
+    private final Connection connection;
 
-    public static Connection getConnection(Properties config) {
+    public SQLiteService(Properties config) {
         try {
-            Connection connection = DriverManager.getConnection(
+            this.connection = DriverManager.getConnection(
                 config.getProperty("sqlite.url")
             );
             System.out.println("Connection to SQLite has been established.");
-            return connection;
         }
         catch (SQLException e) {
             System.err.println(e.getMessage());
             throw new RuntimeException("Connection with SQLlite failed.");
+        }
+    }
+    public Connection getConnection() {
+        System.out.println("Connection to SQLite has been returned.");
+        return this.connection;
+    }
+
+    public void close() {
+        try {
+            if (this.connection != null || !this.connection.isClosed()) {
+                connection.close();
+                System.out.println("Connection to sqlite has been closed.");
+            }
+        }
+        catch (SQLException e) {
+            System.err.printf("Failed closing connection: %s", e.getMessage());
+            throw new RuntimeException(
+                "Failed trying to close connection with sqlite."
+            );
         }
     }
 }
