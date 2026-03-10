b9c49d2ca131a8502c8971601c2beecfa3c2309e
Meromah <124768973+meromah@users.noreply.github.com>
2026-03-10 11:48:39 +0100

    Profile class has been renamed to User;

--- src/com/merge/models/User.java
+++ src/com/merge/models/User.java
@@ -1,45 +1,45 @@
 package com.merge.models;
 
 import java.util.Properties;
-import com.merge.exceptions.ProfileNotFoundException;
-import com.merge.exceptions.ProfileNotFullException;
+import com.merge.exceptions.UserNotFoundException;
+import com.merge.exceptions.UserNotFullException;
 
-public class Profile {
+public class User {
     private String name;
     private String bio;
     private Properties config;
 
-    public Profile(String name, String bio, Properties config) {
+    public User(String name, String bio, Properties config) {
         this.name = name;
         this.bio = bio;
         this.config = config;
     }
 
-    public static Profile load(Properties config) {
+    public static User load(Properties config) {
         String name = config.getProperty("profile.name");
         String bio = config.getProperty("profile.bio");
         if (name == null || name.isBlank()) {
-            throw new ProfileNotFoundException();
+            throw new UserNotFoundException();
         }
-        return new Profile(name, bio, config);
+        return new User(name, bio, config);
     }
 
     public String getName() { return this.name; }
     public String getBio() { return this.bio; }
 
-    public Profile setName(String name) {
+    public User setName(String name) {
         this.name = name;
         return this;
     }
 
-    public Profile setBio(String bio) {
+    public User setBio(String bio) {
         this.bio = bio;
         return this;
     }
 
     public void save() {
         if (this.name == null || this.name.isBlank()) {
-            throw new ProfileNotFullException();
+            throw new UserNotFullException();
         }
         this.config.setProperty("profile.name", this.name);
         this.config.setProperty("profile.bio", this.bio);
